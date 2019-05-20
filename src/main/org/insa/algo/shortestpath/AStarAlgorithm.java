ddcabb8bb0be64de78f886047f82460809a46476
M. Molenda Lucas <molenda@insa-08132>
2019-05-20 08:04:17 +0200

    Clean all

--- src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
@@ -8,15 +8,10 @@ import org.insa.algo.utils.*;
 import org.insa.graph.*;
 
 public class AStarAlgorithm extends DijkstraAlgorithm {
-
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
-        this.nbSommetsVisites = 0;
     }
-	
-	/* Crée et retourne le Label correspondant au Node */
 	protected LabelStar newLabel(Node node, ShortestPathData data) {
 		return new LabelStar(node,data);
 	}
-	
 }
